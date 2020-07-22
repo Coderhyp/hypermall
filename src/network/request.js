de034e735ff0eb7263e7b4fc294382c61730dc92
Coderhyp <68196807+Coderhyp@users.noreply.github.com>
2020-07-22 11:50:14 +0800

    Update request.js

--- src/network/request.js
+++ src/network/request.js
@@ -4,7 +4,7 @@ export function request(config) {
 
   //1.创建axios实例
   const instance = axios.create({
-    baseURL: 'http://152.136.185.210:8000/api/n3',
+    baseURL: '加coderwhy003',
     timeout: 5000
   })
   //2.axios的拦截器
